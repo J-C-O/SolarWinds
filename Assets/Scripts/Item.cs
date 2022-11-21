35ee9337321d6aa962ba2e130384e524786cf9d5
Julius Thiele <47179422+J-C-O@users.noreply.github.com>
2022-11-21 01:41:20 +0100

    inventory prototype

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Item/Create New Item")]
+public class Item : ScriptableObject
+{
+    public int Id;
+    public string ItemName;
+    [SerializeField]
+    private Sprite itemIcon;
+    public int Value;
+
+    public Sprite ItemIcon { get => itemIcon; set => itemIcon = value; }
+}
