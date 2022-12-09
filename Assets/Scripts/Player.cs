6a90d762b545584d7736416cad5bb67b34e4758a
Julius Thiele <47179422+J-C-O@users.noreply.github.com>
2022-12-09 08:57:34 +0100

    added little game manager logic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Properties
+    public string Name;
+    public int PlayerID;
+    public int Points = 0;
+    private bool isActive;
+
+    public Player(string name, int id)
+    {
+        Name = name;
+        PlayerID = id;
+        SetIsActive(false);
+    }
+    public bool GetIsActive()
+    {
+        return isActive;
+    }
+
+    public void SetIsActive(bool value)
+    {
+        isActive = value;
+    }
+}
