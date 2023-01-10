419fad3f2e4822e39bc5285c9f80fcf8a4c7e833
Hendrik <59776927+hensch7@users.noreply.github.com>
2023-01-10 16:35:35 +0100

    added new prefabs for arrows, trash and wind direction

---
+++ Assets/Scripts/WindChange.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindChange : MonoBehaviour
+{
+    public Transform WindConeParent;
+
+    public GameObject WindArrowUp;
+    public GameObject WindArrowDown;
+    public GameObject WindArrowRight;
+    public GameObject WindArrowLeft;
+
+    private int windState = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        incrementWindState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown("w")) {
+            incrementWindState();
+            Debug.Log("Wind moved");
+            Debug.Log(windState);
+        } 
+    }
+
+    void incrementWindState(){
+        if (windState >= 4){
+            windState = 1;
+        } else {
+            windState += 1;
+        }
+        
+        switch (windState) {
+            case 1:
+                foreach (Transform cone in WindConeParent)
+                {
+                    var rotationVector = cone.rotation.eulerAngles;
+                    rotationVector.y = 0;
+                    cone.transform.rotation = Quaternion.Euler(rotationVector);
+                }
+
+                WindArrowUp.SetActive(false);
+                WindArrowDown.SetActive(false);
+                WindArrowRight.SetActive(true);
+                WindArrowLeft.SetActive(false);
+
+                break;
+            case 2:
+                foreach (Transform cone in WindConeParent)
+                {
+                    var rotationVector = cone.rotation.eulerAngles;
+                    rotationVector.y = 90;
+                    cone.transform.rotation = Quaternion.Euler(rotationVector);
+                }
+                WindArrowUp.SetActive(false);
+                WindArrowDown.SetActive(true);
+                WindArrowRight.SetActive(false);
+                WindArrowLeft.SetActive(false);
+                break;
+            case 3:
+                foreach (Transform cone in WindConeParent)
+                {
+                    var rotationVector = cone.rotation.eulerAngles;
+                    rotationVector.y = -90;
+                    cone.transform.rotation = Quaternion.Euler(rotationVector);
+                }
+                WindArrowUp.SetActive(true);
+                WindArrowDown.SetActive(false);
+                WindArrowRight.SetActive(false);
+                WindArrowLeft.SetActive(false);
+                break;
+            case 4:
+                foreach (Transform cone in WindConeParent)
+                {
+                    var rotationVector = cone.rotation.eulerAngles;
+                    rotationVector.y = -180;
+                    cone.transform.rotation = Quaternion.Euler(rotationVector);
+                }
+                WindArrowUp.SetActive(false);
+                WindArrowDown.SetActive(false);
+                WindArrowRight.SetActive(false);
+                WindArrowLeft.SetActive(true);
+                break;
+        }
+    }
+}
